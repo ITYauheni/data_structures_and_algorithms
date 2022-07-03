713f269b5e71d522eb1b1439b19ae035e33a183b
ITYauheni <itkachenkayauheni@gmail.com>
2022-07-04 00:10:19 +0300

    Implemented custom data structure - stack

---
+++ stack.py
@@ -0,0 +1,52 @@
+class Stack:
+    """A stack is an ordered set of data
+    in which the placement of new elements and the removal of existing ones is carried out only at one end of it,
+    which is called the top of the stack.
+
+    principle of "last in first out" (LIFO)
+
+    push(item) - adds the item to the top of stack
+
+    pop() - removes and returns item from the top of the stack"""
+
+    __head = None
+    __length = 0
+
+    class Node:
+        def __init__(self, value, previous=None):
+            self.value = value
+            self.previous = previous
+
+    def push(self, value):
+        """adds the item to the top of stack"""
+        if self.__length == 0:
+            self.__head = self.Node(value)
+        else:
+            previous = self.__head
+            self.__head = self.Node(value, previous)
+
+        self.__length += 1
+
+    def pop(self):
+        """removes and returns item from the top of the stack"""
+        node = self.__head
+        self.__head = self.__head.previous
+        self.__length -= 1
+        return node.value
+
+    def __len__(self):
+        return self.__length
+
+    def __bool__(self):
+        return bool(self.__length)
+
+
+if __name__ == "__main__":
+    stack = Stack()
+    for i in range(10):
+        stack.push(i)
+
+    while stack:
+        print(stack.pop())
+
+
